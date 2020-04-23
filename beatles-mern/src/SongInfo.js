58387c5f099ccb65142111340bde97249e8baaa5
Stu Kelly <sbkelly9@gmail.com>
2020-04-23 19:24:06 -0400

    working state

--- beatles-mern/src/SongInfo.js
+++ beatles-mern/src/SongInfo.js
@@ -2,50 +2,7 @@ import React from "react";
 let url = "https://cors-anywhere.herokuapp.com/https://beatles-api.herokuapp.com"
 
 export default function SongInfo(props) {
-<<<<<<< HEAD
-  console.log(props)
-  fetch(url)
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-    })
-		// 		const html = $('#ex1')
-		// 		const elementIdArray = ["Artist", "Song", "Lyrics"]
-
-		// 		elementIdArray.forEach(elemId => {
-		// 			$(`#${elemId}`).text(`${elemId}: ${SongInfo[elemId]}`)
-		// 		})
-
-		// 		$(html).appendTo('body').modal()
-		// 	});
-		// })
-		// .catch(err => {
-		// 	console.log("something went wrong...", err);
-		// });
-=======
-  // console.log(props)
-  // fetch(url)
-	// 	.then(res => {
-	// 		let response = res.json();{(
-// 			response.then(SongInfo => { 				
-//         event.preventDefault();
-// 				this.blur(); // Manually remove focus from clicked link.
-
-// 				const html = $('#ex1')
-// 				const elementIdArray = ["Artist", "Song", "Lyrics"]
-
-// 				elementIdArray.forEach(elemId => {
-// 					$(`#${elemId}`).text(`${elemId}: ${SongInfo[elemId]}`)
-// 				})
-
-// 				$(html).appendTo('body').modal()
-// 			});
-// 		})
-// 		.catch(err => {
-// 			console.log("something went wrong...", err);
-// 		});
-// }
->>>>>>> origin
+  
   return (
     <div>
       <div>
