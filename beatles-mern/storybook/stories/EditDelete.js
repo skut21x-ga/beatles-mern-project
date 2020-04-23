b16939088c4faa07e5dd6fd8cfd7b7080e606d2a
Work <Work@Scotts-MBP.fios-router.home>
2020-04-23 00:10:20 -0400

    finished edit delete icons. choose one.

--- beatles-mern/storybook/stories/EditDelete.js
+++ beatles-mern/storybook/stories/EditDelete.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./EditDelete.css";
+import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import EditIcon from "@material-ui/icons/Edit";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
 
-<link
-  href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
-  rel="stylesheet"
-></link>;
+//utilized material ui code below
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.palette.text.primary,
+  },
+}));
 
-const EditDelete = (props) => {};
+export default function EditDelete() {
+  const classes = useStyles();
 
-export default EditDelete;
+  return (
+    <Grid container className={classes.root}>
+      <Grid item xs={4}>
+        <Grid item xs={8}>
+          <DeleteIcon className="Icon" /> <EditIcon className="Icon" />
+          {"   "}
+          or {"   "}
+          <DeleteOutlinedIcon className="Icon" />{" "}
+          <EditOutlinedIcon className="Icon" />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
