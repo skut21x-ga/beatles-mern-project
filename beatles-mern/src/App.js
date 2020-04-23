1367bdd72c19c21ac35a538cb81ffe24e96abed8
DAMI KOMOLAFE <damik94@gmail.com>
2020-04-23 15:35:54 -0400

    Modal next

--- beatles-mern/src/App.js
+++ beatles-mern/src/App.js
@@ -1,79 +1,50 @@
-import Modal from "@material-ui/core/Modal";
-import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import "./App.css";
 import SongInfo from "./SongInfo";
-<<<<<<< HEAD
-import NewSong from "./NewSong";
-=======
+//import NewSong from "./NewSong";
 import Navbar from "./Navbar"
->>>>>>> origin
+import Home from "./Home/Home";
+import JohnLennon from './JohnLennon/JohnLennon'
+import PaulMcCartney from './PaulMcCartney/PaulMcCartney'
+import GeorgeHarrison from './GeorgeHarrison/GeorgeHarrison'
+import RingoStarr from './RingoStarr/RingoStarr'
+import { Route, Link } from "react-router-dom";
 
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
 
 function App() {
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const song = {
-    artist: "artistName",
-    song: "songTitle",
-    lyrics: "songLyrics",
-  };
-
-  const dummy = () => console.log("hello")
+  
 
   return (
     <div className="App">
       <Navbar></Navbar>
       {/*  */}
-      <button onClick={handleOpen}>Open</button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        <div style={modalStyle} className={classes.paper}>
-          <SongInfo data={song} onSubmit={dummy} onDelete={dummy} />
-        </div>
-      </Modal>
-
+      <main>
+    <Route
+      path="/"
+      exact
+      component={Home}
+    />
+    <Route
+      path="/JohnLennon/"
+      component={JohnLennon}
+    />
+      <Route
+      path="/PaulMcCartney"
+      exact
+      component={PaulMcCartney}
+    />
+    <Route
+      path="/GeorgeHarrison"
+      exact
+      component={GeorgeHarrison}
+    />
+    <Route
+      path="/RingoStarr"
+      exact
+      component={RingoStarr}
+    />
+   </main>
     </div>
   );
 }
--- beatles-mern/src/SongInfo.js
+++ beatles-mern/src/SongInfo.js
@@ -2,28 +2,28 @@ import React from "react";
 let url = "https://beatles-api.herokuapp.com/"
 
 export default function SongInfo(props) {
-  console.log(props)
-  fetch(url)
-		.then(res => {
-			let response = res.json();
-			response.then(SongInfo => { 				
-        event.preventDefault();
-				this.blur(); // Manually remove focus from clicked link.
+  // console.log(props)
+  // fetch(url)
+	// 	.then(res => {
+	// 		let response = res.json();{(
+// 			response.then(SongInfo => { 				
+//         event.preventDefault();
+// 				this.blur(); // Manually remove focus from clicked link.
 
-				const html = $('#ex1')
-				const elementIdArray = ["Artist", "Song", "Lyrics"]
+// 				const html = $('#ex1')
+// 				const elementIdArray = ["Artist", "Song", "Lyrics"]
 
-				elementIdArray.forEach(elemId => {
-					$(`#${elemId}`).text(`${elemId}: ${SongInfo[elemId]}`)
-				})
+// 				elementIdArray.forEach(elemId => {
+// 					$(`#${elemId}`).text(`${elemId}: ${SongInfo[elemId]}`)
+// 				})
 
-				$(html).appendTo('body').modal()
-			});
-		})
-		.catch(err => {
-			console.log("something went wrong...", err);
-		});
-}
+// 				$(html).appendTo('body').modal()
+// 			});
+// 		})
+// 		.catch(err => {
+// 			console.log("something went wrong...", err);
+// 		});
+// }
   return (
     <div>
       <div>
@@ -37,4 +37,4 @@ export default function SongInfo(props) {
       </div>
     </div>
   );
-}
+  }
