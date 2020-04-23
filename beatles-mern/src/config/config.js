a6dede61ec46387bb7825d33dd9b112185535166
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-04-23 10:23:57 -0400

    updated code fix

--- beatles-mern/src/config/config.js
+++
@@ -1,8 +0,0 @@
-// starter code from https://git.generalassemb.ly/dc-wdi-react-redux/react-jwt-authentication
-
-module.exports = {  
-    jwtSecret: 'JwtS3cr3tK3Y',
-    jwtSession: {
-        session: false
-    }
-}
--- beatles-mern/src/index.js
+++ beatles-mern/src/index.js
@@ -4,25 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const express = require("express");
-const parser = require("body-parser");
-const cors = require("cors");
-const app = express();
-
-// jwt code from https://git.generalassemb.ly/dc-wdi-react-redux/react-jwt-authentication
-
-app.use(cors());
-app.use(parser.json());
-app.listen(3000, () => console.log("Listening on port 3000 :)"));
-
-const passport = require("./config/passport")();
-
-const userController = require("./controllers/users.js");
-
-app.use("/users", userController);
-
-app.use(passport.initialize());
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
