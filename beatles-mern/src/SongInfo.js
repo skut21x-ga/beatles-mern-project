8df702d00b9a4744dff6fbf22f6e413bef1916b9
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-04-24 00:28:21 -0400

    full team syncup

--- beatles-mern/src/SongInfo.js
+++ beatles-mern/src/SongInfo.js
@@ -1,26 +1,16 @@
 import React from "react";
+import "./SongInfo.css";
+
 let url =
   "https://cors-anywhere.herokuapp.com/https://beatles-api.herokuapp.com";
 export default function SongInfo(props) {
   return (
-    <div>
+    <div className="songModal">
       <div>
         <h2 id="Song Title"></h2>
-       
-          <div>{props.value}</div>
-          <div>{props.datavalue}</div>
-       
 
-        <p>
-          <br></br>
-          <br></br>
-          {props.value}
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          {props.datavalue}
-        </p>
+        <div>{props.value}</div>
+        <div>{props.datavalue}</div>
       </div>
     </div>
   );
